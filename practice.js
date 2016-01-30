ed399a103cab6cccdcef7d84daa2657c4de4fe3b
Nate H <Nate H>
2016-01-30 10:11:37 -0700

    commiting file

--- practice.js
+++ practice.js
@@ -5,6 +5,10 @@
 
   //Code here
 
+var me = {
+  name: "Nate",
+  age: 31
+};
 
 
 
@@ -13,21 +17,34 @@
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to 
+//those keys be your favorite thing in that category.
+
+  //Code here
+var favoriteThings = {
+  band: 'Govt Mule',
+  food: 'steak',
+  person: 'Omar',
+  book: 'The One Tree',
+  movie: 'Hedwig',
+  holiday: 'Christmas'
+};
+
+
+//After you've made your object, add another key named 'car' with the value being your favorite car and then another key 
+//named 'brand' with the value being your favorite brand.
+
+  //Code here
+favoriteThings.car = 'Audi';
+favoriteThings.brand = 'Amex';
+
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to 
+//be '50 Shades of Gray'.
 
   //Code here
 
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
-
-  //Code here
-
-
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
-
-  //Code here
-
-
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 
 //NEXT PROBLEM
@@ -43,6 +60,12 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
+var backPack = {};
+var item = 'firstPocket';
+
+backPack[item] = 'chapstick';
+
+backPack.color = 'black';
 
 //After you do the above, alert your entire backPack object.
 
@@ -54,7 +77,7 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code here
 
-
+console.log(backPack);
 
 
 //NEXT PROBLEM
@@ -62,46 +85,75 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
+//Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those 
+//properties in with the appropriate values.
 
   //Code Here
-
+var alsoMe = {
+  name: 'Nate',
+  age: 31,
+  height: '6ft',
+  gender: 'Male',
+  married: 'Married',
+  eyeColor: 'hazel',
+  hairColor: 'brown'
+}
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
-
-
-
+for (var prop in alsoMe) {
+  console.log(alsoMe[prop]);
+}
 
 //NEXT PROBLEM
 
 
 
 
-//Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
+//Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length 
+//of each song.
 
   //Code Here
+var album = {
+  FarAway: '3.15',
+  BlindMan: '6.00',
+  Mule: '8.00',
+  AboutToRage: '2.50',
+  LittleToyBrain: '4.30',
+};
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
-
+for (var prop in album) {
+  alert(prop);
+}
 
 
 
 //NEXT PROBLEM
 
 
-
-
-//Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
-
+//Create an object called states that has 5 US states as properties with the values being their population (doesn't have 
+  //to be accurate).
+var states = {
+  Alabama: 10000,
+  Alaska: 5000,
+  Arizona: 40000,
+  Arkansas: 20000,
+  California: 60000
+}
   //Code Here
+
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
-
+for (var key in states) {
+  if (states[key] > 30000) {
+    alert(key);
+  }
+}
 
 
 
@@ -122,11 +174,17 @@ var user1 = {
 that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
+  for (var key in user1) {
+    if (!user1[key]) delete user1[key];
 
-//Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
+  }
+
+//Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', 
+  //username: 'your username'), rather than my information.
 
   //Code Here
-
+user1.name = 'Nate Hickman';
+user1.username = 'nghickman';
 
 
 
@@ -150,12 +208,15 @@ var user2 = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 //Now call the sayName method that's on the user object which will alert the users email
 
+
   //Code Here
 
-
+user2.sayName();
 
 
 //NEXT PROBLEM
@@ -166,16 +227,24 @@ var user2 = {
 //Create an empty object called methodCollection.
 
   //Code Here
-
+var methodCollection = {}
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
   //Code Here
+methodCollection.alertHello = function() {
+  alert('hello');
+};
 
+methodCollection.logHello = function() {
+  console.log('hello');
+}
 //Now call your alertHello and logHello methods.
 
   //Code Here
+methodCollection.alertHello();
+methodCollection.logHello();
 
 
 
@@ -183,20 +252,34 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 
 
-// Create a function called MakePerson which takes in name, birthday, ssn as its parameters and returns a new object with all of the information that you passed in.
+// Create a function called MakePerson which takes in name, birthday, ssn as its parameters and returns a new object with all 
+//of the information that you passed in.
 
   //Code Here
-
+var MakePerson = function(name, birthday, ssn) {
+  return {
+    name: name,
+    birthday: birthday,
+    ssn: ssn
+  }
+}
 
 
 //NEXT PROBLEM
 
 
 
-// Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
+// Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object 
+//so that whenever you invoke MakeCard, you get a brand new credit card.
 
   //Code Here
-  
+var MakeCard = function(name, number, expiration) {
+  return {
+    name: name,
+    number: number,
+    expiration: expiration
+  }
+}
   
   
 //NEXT PROBLEM
@@ -204,10 +287,21 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 
 /* As of this point you should have a MakePerson and a MakeCard function which returns you either a person or a credit card object.
-   Now, create a bindCard function that takes in a person object as its first parameter and a creditcard object as its second parameter.
-   Have bindCard merge the two parameters together into a new object which contains all the properties from the person as well as the creditcard.
+   Now, create a bindCard function that takes in a person object as its first parameter and a creditcard object as its second 
+   parameter. Have bindCard merge the two parameters together into a new object which contains all the properties from the person as 
+   well as the creditcard.
 */
 
   //Code Here
+var bindCard = function(person, card) {
+  var merge = {};
+  for (var key in person) {
+    merge[key] = person[key];
+  }
+  for (var key in card) {
+    merge[key] = card[key];
+  }
+  return merge;
+}
 
 
